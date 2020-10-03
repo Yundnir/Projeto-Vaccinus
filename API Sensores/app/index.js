31a67eed4ee4aadd6dc218688adbfbafeeefb7c9
renpaulino <renato.paulino@bandtec.com.br>
2020-10-02 22:20:36 -0300

    API Alterada: Tirei as informações dos outros sensores. Mudei a aparência do Dashboard; alterei a escala do eixo X e Y do Dashboard. Editei o arquivo da LOGO

--- API Sensores/app/index.js
+++ API Sensores/app/index.js
@@ -11,36 +11,7 @@ temperatura = sensors.lm35(10,15)
 incrementando com uma string para facilitar a interpretação.*/
 console.log('Temperatura: ' + temperatura.toFixed(2))
 
-//Atribuindo o retorno da função trc5000 a variável chave.
-chave = sensors.trc5000()
-
-//Exibindo o valor retornado da função na variável.
-console.log('Switch: ' + chave)
 
 
-//Atribuindo o retorno da função ldr a variável ldr.
-ldr = sensors.ldr()
 
-//Exibindo o valor retornado da função na variável.
-console.log('Luz: ' + ldr)
-
-//Atribuindo o retorno da função dht11 a variável dht11.
-/*
-Nesta função, é possível definir os valores limites de mínimo e máximo para temperatura e
-umidade relativa, através do envio de um objeto como parametro para função.
-São os atributos deste objeto: 
-- minHum: valor minimo de umidade
-- maxHum: valor máximo de umidade
-- minTemp: valor máximo de temperatura
-- maxTemp: valor minimo de temperatura
-Exemplo:
-case1 = {minHum: 20 , maxHum: 80, minTemp: 20, maxTemp: 25}
-*/
-dht11 = sensors.dht11({minHum:50, maxHum:100, minTemp: 18, maxTemp: 23})
-
-
-//Exibindo o valor retornado da função na variável.
-
-console.log('DHT11 - Umidade: ' + dht11[0])
-console.log('DHT11 - Temperatura: ' + dht11[1].toFixed(2))
 
