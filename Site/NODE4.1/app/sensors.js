7479de85eaa1f43909e606821b153c66cbcd9e24
Shoyo <shoyo@hotmail.com>
2020-11-07 17:55:06 -0300

    homologacao

--- Site/NODE4.1/app/sensors.js
+++ Site/NODE4.1/app/sensors.js
@@ -1,6 +1,6 @@
 function lm35(min, max) {
-    min = typeof min == 'undefined' ? 0 : min;
-    max = typeof max == 'undefined' ? 10 : max;
+    min = typeof min == 'undefined' ? 2 : min;
+    max = typeof max == 'undefined' ? 8 : max;
     
     let random = Math.random() * (max - min) + min;
 
